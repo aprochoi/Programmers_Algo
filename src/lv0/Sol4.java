c5e62e237a49d3fe322aa2cd9aaf7f46d241a5bc
aprochoi <ym24319577@gmail.com>
2022-10-24 18:40:56 +0900

    Lv.0 피자나눠먹기(1)

---
+++ src/lv0/Sol4.java
@@ -0,0 +1,23 @@
+package lv0;
+
+public class Sol4 {
+    public static void main(String[] args) {
+        System.out.println(solution(7)); // 1
+        System.out.println(solution(1)); // 1
+        System.out.println(solution(15)); // 3
+        System.out.println(solution(14)); // 2
+    }
+
+    public static int solution(int n) {
+
+//        if (n <= 7) {
+//            return 1;
+//        } else if (n % 7 == 0) {
+//            return n / 7;
+//        }
+//        else return n/7+1;
+
+        return n % 7 == 0 ? n/7 : n/7+1;
+
+    }
+}
