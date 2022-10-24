afb4e8e3616a5dd2f5e60d3ecc3825d508aa40f1
aprochoi <ym24319577@gmail.com>
2022-10-24 18:08:00 +0900

    Lv.0 짝수는싫어요

---
+++ src/lv0/Sol3.java
@@ -0,0 +1,26 @@
+package lv0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Sol3 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(10)));
+        System.out.println(Arrays.toString(solution(15)));
+    }
+
+    public static int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                arr.add(i);
+            }
+        }
+        System.out.println(arr.toString());
+
+        int[] ints = arr.stream().mapToInt(Integer::intValue).toArray();
+
+        return ints;
+    }
+}
