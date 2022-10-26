f24e3d4ca41ee759c8cfb98f60b9fd6b3723b50e
aprochoi <ym24319577@gmail.com>
2022-10-26 18:04:09 +0900

    나이 출력

---
+++ src/lv0/Sol10.java
@@ -0,0 +1,12 @@
+package lv0;
+
+public class Sol10 {
+    public static void main(String[] args) {
+        System.out.println(solution(40));
+        System.out.println(solution(23));
+    }
+
+    public static int solution(int age) {
+        return 2022-age+1;
+    }
+}
