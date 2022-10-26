bdee91d3afd51017dd36633cb7abdd0393ad2dc6
aprochoi <ym24319577@gmail.com>
2022-10-26 17:57:43 +0900

    아이스 아메리카노

---
+++ src/lv0/Sol9.java
@@ -0,0 +1,22 @@
+package lv0;
+
+import java.util.Arrays;
+
+public class Sol9 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(5500)));
+        System.out.println(Arrays.toString(solution(15000)));
+    }
+
+    public static int[] solution(int money) {
+        int[] res = new int[2];
+
+        while (money >= 5500) {
+            money -= 5500;
+            res[0]++;
+        }
+        res[1] = money;
+
+        return res;
+    }
+}
