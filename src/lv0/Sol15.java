091563c08ce6ddb5fbe0a0e0ebeebe1e65d08825
aprochoi <ym24319577@gmail.com>
2022-10-27 14:38:44 +0900

    문자 반복 출력하기

---
+++ src/lv0/Sol15.java
@@ -0,0 +1,17 @@
+package lv0;
+
+public class Sol15 {
+    public static void main(String[] args) {
+        System.out.println(solution("hello", 3));
+    }
+
+    public static String solution(String my_string, int n) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < my_string.length(); i++) {
+            res.append(String.valueOf(my_string.charAt(i)).repeat(n));
+        }
+
+        return res.toString();
+    }
+}
