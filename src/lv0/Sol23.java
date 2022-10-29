a8a1bb55df566d6212f6166e7dc38b83911aa561
aprochoi <ym24319577@gmail.com>
2022-10-29 15:09:12 +0900

    순서쌍의 개수 stream으로

--- src/lv0/Sol23.java
+++ src/lv0/Sol23.java
@@ -1,5 +1,7 @@
 package lv0;
 
+import java.util.stream.IntStream;
+
 public class Sol23 {
     public static void main(String[] args) {
         System.out.println(solution(20));
@@ -7,18 +9,21 @@ public class Sol23 {
     }
 
     public static int solution(int n) {
-        int res = 0;
+//        int res = 0;
+//
+//
+//        double sqrt = Math.sqrt(n);
+//
+//        for (int i = 1; i < sqrt; i++) {
+//            if(n % i == 0) res += 2;
+//        }
+//
+//        if(n % sqrt == 0) res++;
+//
+//
+//        return res;
 
-
-        double sqrt = Math.sqrt(n);
-
-        for (int i = 1; i < sqrt; i++) {
-            if(n % i == 0) res += 2;
-        }
-
-        if(n % sqrt == 0) res++;
-
-
-        return res;
+        //stream 형식으로 풀이.
+        return (int) IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count();
     }
 }
