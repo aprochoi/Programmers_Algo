044eff329b990860b63d6251dfcb89c630d3f6bd
aprochoi <ym24319577@gmail.com>
2022-10-30 14:04:05 +0900

    구슬을 나누는 경우의 수

---
+++ src/lv0/Sol27.java
@@ -0,0 +1,23 @@
+package lv0;
+
+import java.math.BigInteger;
+
+public class Sol27 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 2));
+        System.out.println(solution(5, 3));
+    }
+
+    public static long solution(int balls, int share) {
+        share = Math.min(balls - share, share);
+
+        if (share == 0)
+            return 1L;
+
+        long result = solution(balls - 1, share - 1);
+        result *= balls;
+        result /= share;
+
+        return result;
+    }
+}
