d1f89a472f8d87e4f15e6ed4dd16228ebbabe0d6
aprochoi <ym24319577@gmail.com>
2022-10-31 13:07:10 +0900

    2차원으로 만들기

---
+++ src/lv0/Sol29.java
@@ -0,0 +1,26 @@
+package lv0;
+
+import java.util.Arrays;
+
+public class Sol29 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 2)));
+        System.out.println(Arrays.deepToString(solution(new int[]{100,95,2,4,5,6,18,33,948}, 3)));
+    }
+
+    public static int[][] solution(int[] num_list, int n) {
+        int[][] res = new int[num_list.length/n][n];
+
+        int index = 0;
+
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = num_list[index];
+                index++;
+            }
+        }
+
+
+        return res;
+    }
+}
