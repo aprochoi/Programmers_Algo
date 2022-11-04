cad3391dbdeb782b3e3a5f5a05ad07d5ee9a107b
aprochoi <ym24319577@gmail.com>
2022-11-04 13:12:39 +0900

    암호 해독

---
+++ src/lv0/Sol46.java
@@ -0,0 +1,18 @@
+package lv0;
+
+public class Sol46 {
+    public static void main(String[] args) {
+        System.out.println(solution("dfjardstddetckdaccccdegk", 4));
+        System.out.println(solution("pfqallllabwaoclk", 2));
+    }
+
+    public static String solution(String cipher, int code) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = code-1; i < cipher.length(); i += code) {
+            res.append(cipher.charAt(i));
+        }
+
+        return res.toString();
+    }
+}
