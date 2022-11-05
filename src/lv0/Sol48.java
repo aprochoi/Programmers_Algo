2a8547ea16531e0a71666705c8d3976b0402f237
aprochoi <ym24319577@gmail.com>
2022-11-05 18:46:26 +0900

    영어가 싫어요

---
+++ src/lv0/Sol48.java
@@ -0,0 +1,27 @@
+package lv0;
+
+import java.util.ArrayList;
+
+public class Sol48 {
+    public static void main(String[] args) {
+        System.out.println(solution("onetwothreefourfivesixseveneightnine"));
+        System.out.println(solution("onefourzerosixseven"));
+    }
+
+    public static long solution(String numbers) {
+
+        String res = numbers.replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9")
+                .replace("zero", "0");
+
+
+        return Long.parseLong(res);
+    }
+}
