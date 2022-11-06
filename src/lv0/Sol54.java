749ded527feb8235cfca316356f9c674e86e6831
aprochoi <ym24319577@gmail.com>
2022-11-06 15:55:27 +0900

    문자열 계산하기

---
+++ src/lv0/Sol54.java
@@ -0,0 +1,26 @@
+package lv0;
+
+import java.util.Arrays;
+
+public class Sol54 {
+    public static void main(String[] args) {
+        System.out.println(solution("3 + 4"));
+        System.out.println(solution("3 + 4 - 8 + 2"));
+    }
+
+    public static int solution(String my_string) {
+        String[] arr = my_string.split(" ");
+        int sum = Integer.parseInt(arr[0]);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("+")) {
+                sum += Integer.parseInt(arr[i + 1]);
+            }
+            if (arr[i].equals("-")) {
+                sum -= Integer.parseInt(arr[i + 1]);
+            }
+        }
+
+        return sum;
+    }
+}
