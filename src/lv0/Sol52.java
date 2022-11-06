0bb29859afa4a4c9b9b4a1c87a978d26445432f1
aprochoi <ym24319577@gmail.com>
2022-11-06 15:40:09 +0900

    편지

---
+++ src/lv0/Sol52.java
@@ -0,0 +1,13 @@
+package lv0;
+
+public class Sol52 {
+    public static void main(String[] args) {
+        System.out.println(solution("happy birthday!"));
+        System.out.println(solution("I love you~"));
+    }
+
+    public static int solution(String message) {
+
+        return message.length() * 2;
+    }
+}
