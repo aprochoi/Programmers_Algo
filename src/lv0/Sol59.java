4cf9cac9532a1da9c26baf85c447cf33dccea6fa
aprochoi <ym24319577@gmail.com>
2022-11-07 11:22:26 +0900

    OX퀴즈

--- src/lv0/Sol59.java
+++ src/lv0/Sol59.java
@@ -13,19 +13,28 @@ public class Sol59 {
     }
 
     public static String[] solution(String[] quiz) throws ScriptException {
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByName("JavaScript");
-
-        ArrayList<String> res = new ArrayList<>();
-
+//        ScriptEngineManager mgr = new ScriptEngineManager();
+//        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+//
+//        ArrayList<String> res = new ArrayList<>();
+//
+//        for (int i = 0; i < quiz.length; i++) {
+//            String[] s = quiz[i].split("=");
+//                if ((int)engine.eval(s[0]) == Integer.parseInt(s[1].trim())) {
+//                    res.add("O");
+//                } else res.add("X");
+//        }
+//
+//
+//        return res.stream().toArray(String[]::new);
+        ArrayList<String> result = new ArrayList<>();
         for (int i = 0; i < quiz.length; i++) {
-            String[] s = quiz[i].split("=");
-                if ((int)engine.eval(s[0]) == Integer.parseInt(s[1].trim())) {
-                    res.add("O");
-                } else res.add("X");
+            String[] s = quiz[i].split(" ");
+            int res = Integer.parseInt(s[0]) + (Integer.parseInt(s[2]) * (s[1].equals("+") ? 1 : -1));
+
+            result.add(res == Integer.parseInt(s[4]) ? "O" : "X");
         }
 
-
-        return res.stream().toArray(String[]::new);
+        return result.toArray(String[]::new);
     }
 }
