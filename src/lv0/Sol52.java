d557ccdb47cccd8a9983892495a2c3a82c10f374
aprochoi <ym24319577@gmail.com>
2022-11-07 10:31:01 +0900

    숫자 찾기

---
+++ src/lv0/Sol52.java
@@ -0,0 +1,21 @@
+package lv0;
+
+public class Sol52 {
+    public static void main(String[] args) {
+        System.out.println(solution(29183, 1));
+        System.out.println(solution(232443, 4));
+        System.out.println(solution(123456, 7));
+    }
+
+    public static int solution(int num, int k) {
+        String res = String.valueOf(num);
+
+        if (!res.contains(String.valueOf(k))) {
+            return -1;
+        }
+
+        int index = res.indexOf(String.valueOf(k));
+
+        return index + 1;
+    }
+}
