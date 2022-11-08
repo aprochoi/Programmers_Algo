8507dd44dcc764e035a4a0df613a919547448637
aprochoi <ym24319577@gmail.com>
2022-11-08 14:33:40 +0900

    7의 개수

---
+++ src/lv0/Sol64.java
@@ -0,0 +1,21 @@
+package lv0;
+
+public class Sol64 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{7, 77, 17}));
+        System.out.println(solution(new int[]{10, 29}));
+    }
+
+    public static int solution(int[] array) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            String s = String.valueOf(array[i]);
+            for (int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) == '7') count++;
+            }
+        }
+
+        return count;
+    }
+}
