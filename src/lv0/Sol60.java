0cb8e0ffec5f53ddfa9dadc6f81bba851eeb176a
aprochoi <ym24319577@gmail.com>
2022-11-08 14:11:55 +0900

    문자열안에 문자열

---
+++ src/lv0/Sol60.java
@@ -0,0 +1,17 @@
+package lv0;
+
+public class Sol60 {
+    public static void main(String[] args) {
+        System.out.println(solution("ab6CDE443fgh22iJKlmn1o", "6CD"));
+        System.out.println(solution("ppprrrogrammers", "pppp"));
+
+    }
+
+    public static int solution(String str1, String str2) {
+        if (str1.contains(str2)) {
+            return 1;
+        }
+
+        return 2;
+    }
+}
