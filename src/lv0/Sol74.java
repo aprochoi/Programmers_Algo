8f4c0d0b63b0a52d9c907c6c9828f4f5ec221bcc
aprochoi <ym24319577@gmail.com>
2022-11-10 15:21:35 +0900

    삼각형의 완성조건(2)

---
+++ src/lv0/Sol74.java
@@ -0,0 +1,27 @@
+package lv0;
+
+import java.util.Arrays;
+
+public class Sol74 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2}));
+        System.out.println(solution(new int[]{3, 6}));
+        System.out.println(solution(new int[]{11, 7}));
+    }
+
+    static int solution(int[] sides) {
+        int count = 0;
+        Arrays.sort(sides);
+        int max = sides[1];
+        int sideSum = sides[0] + sides[1];
+
+        for (int i = 1; i < sideSum; i++) {
+            int longLeg = sides[0] + i;
+            if ((max < longLeg && longLeg <= sideSum) || (i > max)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
